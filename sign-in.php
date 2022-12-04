bafda724be3a498deb4d0726bbf93bcb255563f8
oeL niveK <kevinleonardo961@gmail.com>
2022-12-05 00:11:12 +0700

    sign in updated

---
+++ sign-in.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Page Title Icon -->
+        <link rel="shortcut icon" href="img/belajarsantailogo.ico" type="image/x-icon">
+
+        <!-- Bootstrap's CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <!-- Bootstrap's JS Bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+        <style>
+            body {
+                background-color: #3590F3;
+                font-family: Montserrat;
+                font-size: 100%;
+            }
+
+        
+        </style>
+    </head>
+    <body class="d-flex text-center">
+        <div class="container-fluid main_container d-flex text-center">
+            <div id="main-box">
+                <h1>Hahhahahihihuhuhu</h1>
+            </div>
+        </div>
+    </body>
+</html>
