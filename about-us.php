a079132bef22dbd98faac5ac2a42d61f59f1ae1d
Calista <calista.chan02@gmail.com>
2022-12-08 23:47:04 +0700

    New Updates

--- about-us.php
+++ about-us.php
@@ -17,8 +17,8 @@
         }
         #company-profile {
             font-weight: 500;
-            font-size: 20px;
-            line-height: 24px;
+            font-size: 1.25rem;
+            line-height: 1.5rem;
             text-align: center;
             margin: 1rem 0 0 0;
             padding: 0;
@@ -31,20 +31,70 @@
         }
         #vision, #mission {
             font-weight: 700;
-            font-size: 20px;
-            line-height: 24px;
+            font-size: 1.25rem;
+            line-height: 1.5rem;
             text-align: center;
         }
         #vision-detail, #mission-detail {
             font-weight: 500;
-            font-size: 16px;
-            line-height: 20px;
+            font-size: 1rem;
+            line-height: 1.25rem;
             text-align: center;
+        }
+        .contact-us {
+            margin: 3rem 0 0 0;
+        }
+        #more-info, #join-discord {
+            font-weight: 700;
+            font-size: 1.25rem;
+            line-height: 1.5rem;
+            padding: 0;
+            margin: 0 0 1rem 0;
+        }
+        #join-discord {text-align: center;}
+        .contact-info {
+            margin: 0 0 0.5rem 0;
+        }
+        #email-link, #ig-link {
+            font-weight: 600;
+            font-size: 1rem;
+            line-height: 1.25rem;
+            padding: 0;
+            margin: 0 0 0 0.5rem;
+        }
+        a#email-link, a#ig-link {
+            color: #000000;
+            text-decoration: none;
         }
         #discord-button {
             width: 17.5rem;
             height: 4rem;
             border-radius: 0.625rem;
+            background-color: #F39835;
+            color: #FFFFFF;
+            border: none;
+        }
+        #discord-button:hover{
+            color: #D9D9D9;
+        }
+        #discord-button #discord-icon-default {
+            display: inline-block;
+        }
+        #discord-button:hover #discord-icon-default {
+            display: none;
+        }
+        #discord-button #discord-icon-hover {
+            display: none;
+        }
+        #discord-button:hover #discord-icon-hover {
+            display: inline-block;
+        }
+        #discord-text {
+            font-weight: 700;
+            font-size: 1rem;
+            line-height: 1.25rem;
+            display: inline-block;
+            margin: 0 0 0 1rem;
         }
     </style>
 </head>
@@ -73,6 +123,32 @@
             <div class="col-3 logo-pic">
                 <h2 id="mission">MISI</h2>
                 <p id="mission-detail">Memberikan perkenalan terhadap statistika, metode penelitian yang dapat dilakukan, serta pemahaman betapa pentingnya statistika dalam penelitian</p>
+            </div>
+        </div>
+
+        <div class="row contact-us d-flex justify-content-center">
+            <div class="col-4 logo-pic">
+                <h2 id="more-info">Untuk informasi lebih lanjut</h2>
+                <div class="contact-info">
+                    <img src="img/email-icon.svg" alt="Email" style="width: initial; height: initial;">
+                    <a id="email-link" href="mailto:belajarsantai@gmail.com">belajarsantai@gmail.com</a>
+                </div>
+                <div class="contact-info">
+                    <img src="img/ig-icon.svg" alt="Email" style="width: initial; height: initial;">
+                    <a href="#" id="ig-link">@belajarsantai</a>
+                </div>
+            </div>
+            <div class="col-4 logo-pic">
+                <h2 id="join-discord">Masuk komunitas BelajarSantai!</h2>
+                <div class="d-flex justify-content-center">
+                    <button id="discord-button" class="d-flex justify-content-center align-items-center" onclick="location.href='#'">
+                        <div id="discord-icon">
+                            <img id="discord-icon-default" src="img/discord-icon-default.svg" alt="Discord Icon">
+                        <img id="discord-icon-hover" src="img/discord-icon-hover.svg" alt="Discord Icon">
+                        </div>
+                        <p id="discord-text">Discord BelajarSantai</p>
+                    </button>
+                </div>
             </div>
         </div>
     </main>
